a90ff232cd524fd96b130b947ce7f4707f4a03aa
codewilling <codewilling@gmail.com>
2021-02-28 10:49:01 -0800

    add job filtering

--- worker/tasks/fetchGithub.js
+++ worker/tasks/fetchGithub.js
@@ -12,21 +12,22 @@ const baseUrl = 'https://jobs.github.com/positions.json';
 async function fetchGithub(){
 
     console.log('fetching github');
-    let resultCount = 50;
+    let resultCount = 1, onPage = 0;
     const allJobs = [];
 
     //fetch all pages
-    while(resultCount === 50){
-        const res = await fetch(baseUrl);
+    while(resultCount > 0){
+        const res = await fetch(`${baseUrl}?page=${onPage}`);
         const jobs = await res.json();
         allJobs.push(...jobs);
-        resultCount = allJobs.length;
-        console.log(`got`, allJobs.length, `total jobs`, `!`);
+        resultCount = jobs.length;
+        console.log(`got`, resultCount, `jobs!`); 
+        onPage++;
     }
+    console.log(`Got`, allJobs.length, `jobs total!`)
     // filter results
     const jrJobs = allJobs.filter(job => {
         const jobTitle = job.title.toLowerCase();
-        let isJunior = true;
 
         if(jobTitle.includes('senior') ||
             jobTitle.includes('manager') ||
@@ -36,18 +37,20 @@ async function fetchGithub(){
             return false;
         }
 
-        return isJunior;
+        return true;
         
     });
 
     console.log('filtered down to: ', jrJobs.length);
 
-    //set in redis
     
-
+    
+    //save in redis
     const succcess = await asyncSet('github', JSON.stringify(jrJobs));
 
     console.log({succcess});
 }
+
+fetchGithub();
 
 module.exports = fetchGithub;
